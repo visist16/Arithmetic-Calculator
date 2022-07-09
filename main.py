e89a1f8913a7f213b7905624ea660375c25a80f5
Visist Tallam <57991461+visist16@users.noreply.github.com>
2022-07-09 12:23:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from arithmetic_arranger import arithmetic_arranger
 from unittest import main
 
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
 # Run unit tests automatically
